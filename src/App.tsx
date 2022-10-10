69d4f3a3d3153285bb9fdbb906ca9d4c1ea2e57b
fmbellomy <fmbellomy@quantumgarbage.com>
2022-10-09 21:48:10 -0500

    this is a nightmare.

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,59 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
-
+import { appWindow } from "@tauri-apps/api/window";
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+  const image_frame_style = {
+    boxShadow: '0 0 10px var(--ctp-mocha-mauve)',
+    width: '40vw',
+    height: '40vh',
+    gridRow: '1'
+  }
+  const image_style = {
+    width: 'inherit',
+    height: 'inherit'
+  }
+  const grid_style = {
+    paddingTop: '5vh',
+    display: 'grid',
+    justifyContent: 'center',
+    gridColumnGap: '7.5vw'
+  }
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
     setGreetMsg(await invoke("greet", { name }));
   }
-
+  document
+    .getElementById("titlebar-minimize")?.addEventListener("click", () => appWindow.minimize());
+  document
+    .getElementById("titlebar-maximize")?.addEventListener("click", () => appWindow.toggleMaximize());
+  document
+    .getElementById("titlebar-close")?.addEventListener("click", () => appWindow.close());
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
+    <div data-tauri-drag-region className="container">
+      <h1>Image to MC</h1>
+      <div className="grid" style={grid_style}>
 
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
+        <div className="image_frame" id="drop_zone" onDrop={(event) => {
+          event.preventDefault(); event.stopPropagation(); for (const f of event.dataTransfer.files) {
+            setImage(URL.createObjectURL(f));
+            console.log('filename of dragged file: ', f.name);
+          }
+        }} onDragOver={(e) => { e.preventDefault(); e.stopPropagation(); }} style={image_frame_style}>
+          <img src={image} style={image_style}></img>
 
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <div className="row">
-        <div>
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="button" onClick={() => greet()}>
-            Greet
-          </button>
         </div>
+        <div className="image_frame" id="preview" style={image_frame_style}>
+
+        </div>
+
       </div>
-      <p>{greetMsg}</p>
-    </div>
+    </div >
   );
 }
 
